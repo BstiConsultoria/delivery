1aa0dd49b558e990b5c8de3cbce3d0f0e4f715b6
SILVA.ext Luis <Luis.SILVA.ext@sodexo.com>
2016-12-13 18:44:28 -0200

    Server Node

---
+++ config/express.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var load = require('express-load');
+var bodyParser = require('body-parser');
+
+module.exports = function () {
+	var app = express();
+	app.set('port', 3000);
+	app.use(express.static('./public'));
+	app.set('view engine', 'ejs');
+	app.set('views', './views');
+	app.use(bodyParser.urlencoded({extended: true}));
+	app.use(bodyParser.json());
+	app.use(require('method-override')());
+	
+	load('models').then('controllers').then('routes').into(app);
+
+	return app;
+};
+
